899781a681dbb698b79315cdd5ccbbc536fb5b9e
Arne Caluwe <arne.caluwe@outlook.com>
2023-09-05 12:11:28 +0200

    type jokes service and remove unnessary headers

--- src/app/services/jokes.service.ts
+++ src/app/services/jokes.service.ts
@@ -8,15 +8,23 @@ export class JokesService {
   private _httpClient = inject(HttpClient);
   private _config = inject(APP_CONFIG);
 
-  getJokes$(): Observable<any> {
-    return this._httpClient.get(`${this._config.apiEndpoint}joke/Any`, {
+  getJokes$(): Observable<Joke> {
+    return this._httpClient.get<Joke>(`${this._config.apiEndpoint}joke/Any`, {
       params: {
         format: 'json',
         type: 'single',
-      },
-      headers: {
       },
     });
   }
   constructor() {}
 }
+
+export type Joke = {
+  error: boolean;
+  category: string;
+  type: 'single';
+  joke: string;
+  id: number;
+  safe: boolean;
+  lang: string;
+};
